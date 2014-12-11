3a9e41aa298ae7a2d39a4f63c492a85b4376298c
Heiko Theuner <heiko.theuner@gmail.com>
2014-12-10 22:46:35 -0800

    simple setup for a label

--- NSAttributedString-Markdown/HTHViewController.m
+++ NSAttributedString-Markdown/HTHViewController.m
@@ -10,6 +10,8 @@
 
 @interface HTHViewController ()
 
+@property (nonatomic) UILabel *label;
+
 @end
 
 @implementation HTHViewController
@@ -19,6 +21,54 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        label.numberOfLines = 0;
+        label.text = @"s";
+        
+        self.label = label;
+    })];
+    
+    [self applyContraints];
+}
+
+- (void)applyContraints
+{
+    [self.label.superview addConstraint:({
+        [NSLayoutConstraint
+         constraintWithItem: self.label
+         attribute: NSLayoutAttributeCenterX
+         relatedBy: NSLayoutRelationEqual
+         toItem: self.label.superview
+         attribute: NSLayoutAttributeCenterX
+         multiplier: 1.0
+         constant: 0];
+    })];
+    
+    [self.label.superview addConstraint:({
+        [NSLayoutConstraint
+         constraintWithItem: self.label
+         attribute: NSLayoutAttributeCenterY
+         relatedBy: NSLayoutRelationEqual
+         toItem: self.label.superview
+         attribute: NSLayoutAttributeCenterY
+         multiplier: 1.0
+         constant: 0];
+    })];
+    
+    [self.label.superview addConstraint:({
+        [NSLayoutConstraint
+         constraintWithItem: self.label
+         attribute: NSLayoutAttributeWidth
+         relatedBy: NSLayoutRelationEqual
+         toItem: self.label.superview
+         attribute: NSLayoutAttributeWidth
+         multiplier: 3.f / 4.f
+         constant: 0];
+    })];
+    
 }
 
 @end
